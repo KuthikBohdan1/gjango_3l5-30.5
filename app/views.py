ead9456de222ce1235f2de65f7afd7677018d93e
KuthikBohdan1 <bkuzik14@gmail.com>
2025-05-31 10:49:53 +0300

    all_poducts

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from app.models import Post
 # Create your views here.
+
+
+def all_posts(request):
+    posts = Post.object.all()
+
+    context = {
+       "all_posts": all_posts
+
+   }
+    return render(
+        request,
+        template_name="app/all_poducts.html",
+        context=context, 
+
+    )
